fe7856e0188c64cfed8cf0dfcd07387929ec0edf
lm0x86 <liviu.maftuleac@opentech.dev>
2024-03-13 13:43:56 +0200

    - sper ca asta ii tat

--- public/bots/bots-plugin.js
+++ public/bots/bots-plugin.js
@@ -9,9 +9,7 @@
         id: '2e6aa58c-683a-42c7-83fc-30ade0d8a67c',
         name: 'Willy the Shaky',
         details: `
-          Speaks old English
-          Has a role of Customer Support
-          Knows all about Fineguide products
+          Speaks old English, a distinguished gentleman indeed!
         `
       },
       {
@@ -19,9 +17,7 @@
         id: '48ec5509-b301-4bcb-9263-415e7bf5adec',
         name: 'Yoda',
         details: `
-        Speaks old English
-        Has a role of Customer Support
-        Knows all about Fineguide products
+        Speaks Yodaish, knows how to use the Fineguide Force
       `
       },
       {
@@ -29,45 +25,35 @@
       id: '2e90d7ea-0e1f-4e78-8d87-786bbedfb8bb',
       name: 'Jack, The Scotsman',
       details: `
-      Speaks old English
-      Has a role of Customer Support
-      Knows all about Fineguide products
+      Speaks with a distinctive Scottish accent, exuding a touch of authentic Scots charm.
     `
     }, {
       imgUrl: 'https://s3.us-east-2.amazonaws.com/com.publicfiles.aibot/faff609f-d783-4ff0-a068-807fac667a9c/avatars/eddy.jpeg',
       id: '2d940077-6a7a-4227-93a6-6e3345caa8d8',
       name: 'Eddy A. Poe',
       details: `
-      Speaks old English
-      Has a role of Customer Support
-      Knows all about Fineguide products
+      In the shadowy realm of textual discourse, thy chatbot doth converse, a spectral warden of words
     `
     }, {
       imgUrl: 'https://s3.us-east-2.amazonaws.com/com.publicfiles.aibot/faff609f-d783-4ff0-a068-807fac667a9c/avatars/_731867c0-417f-496c-9f33-0f32c7dc361d.jpeg',
       id: '0f4aa93b-c607-4281-a018-bb2930c61522',
-      name: 'Ice-T',
+      name: 'Ice-Vanila',
       details: `
-      Speaks old English
-      Has a role of Customer Support
-      Knows all about Fineguide products
+      Yo! let's kick it! 
     `
     }, {
       imgUrl: 'https://s3.us-east-2.amazonaws.com/com.publicfiles.aibot/faff609f-d783-4ff0-a068-807fac667a9c/avatars/d3eddc9c-886a-4d1f-b8d2-f92dd1404132.jpeg',
       id: 'ea64ae71-462a-4e1c-93b5-dcbe1b78ab44',
       name: 'Pirate Wilds',
       details: `
-      Speaks old English
-      Has a role of Customer Support
-      Knows all about Fineguide products
+        Yarr matey! Your chatbot be chattin' in true pirate fashion, with a seafaring tongue and a touch of swashbucklin' flair. Arr!
     `
     },{
       imgUrl: 'https://s3.us-east-2.amazonaws.com/com.publicfiles.aibot/faff609f-d783-4ff0-a068-807fac667a9c/avatars/0ec3bcee-a66b-4872-810c-1ad6c663c834.jpeg',
       id: 'ad0a130b-24b2-4199-bcdf-35d8d7ced770',
       name: 'Wilford',
       details: `
-      Speaks old English
-      Has a role of Customer Support
-      Knows all about Fineguide products
+      Your chatbot talks like a true cowboy, drawlin' with a Western twang, as if it just rode in from the frontier. Yeehaw!
     `
     }];
 
---
+++ todo.md
@@ -0,0 +1,18 @@
+## TODOS!
+
+- remove menu animations
+- pina maine facem un loom
+- graphy.app
+- Post on Linkedin
+
+1. Betalist.com - startups listing
+2. Produ
+
+
+- Hi, i've been queit for a a while, we've been busy
+
+
+
+
+
+
